9a4b14fdfef1056e64d43aa859e9d72989660338
Robins-2603 <robinsforwork@gmail.com>
2022-07-13 22:57:33 +0530

    Added:Refacor code

--- src/main/java/com/moodAnalyser/MoodAnalyser.java
+++ src/main/java/com/moodAnalyser/MoodAnalyser.java
@@ -1,19 +1,19 @@
 package com.moodAnalyser;
 
 public class MoodAnalyser {
-    private final String message;
-
-    public MoodAnalyser(String message) {
+    public static String message;
+    //constructor
+    public MoodAnalyser(String message){
         this.message = message;
         analyseMood();
     }
-    public String analyseMood() {
+    public String analyseMood(){
         if (message.contains("Sad"))
-            return "Sad";
-//        else if (message.contains("Happy"))
-//            return "HAPPY";
+            return "SAD";
+        else if (message.contains("Happy"))
+            return "HAPPY";
         else
-            return "Happy";
+            return "HAPPY";
 
     }
 }
